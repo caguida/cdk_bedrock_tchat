dd7a32aec4732cf28ef83a5cf458bf6541a9dd37
caguida <calebcarloss@gmail.com>
2023-09-25 17:46:42 +0200

    delete draft

--- tests/test.py
+++
@@ -1,9 +0,0 @@
-import aws_cdk as core
-import aws_cdk.assertions as assertions
-
-from stack.web_app_stack import GenerativeAiDemoWebStack
-
-# example tests. To run these tests, uncomment this file along with the example
-# resource in generative_ai_sagemaker_cdk_demo/generative_ai_sagemaker_cdk_demo_stack.py
-def test_sqs_queue_created():
-    app = core.App()
